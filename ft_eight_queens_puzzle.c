fad90d001aac60f9bee6acff6e3844abd83aecd3
peter_luu <peter.luu510@gmail.com>
2017-01-04 15:21:22 -0800

    added header file

--- ft_eight_queens_puzzle.c
+++ ft_eight_queens_puzzle.c
@@ -1,30 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define DIM 8
-#define N_QUEENS 8
-
-typedef struct coord
-{
-	int x;
-	int y;
-} point;
-
-point *curr_soln_coords;
-int curr_soln_pos;
-point *queen_coords;
-int queen_coord_pos;
-int num_queens;
-
-void render(int **grid);
-void init_grid(int **grid);
-int solve_queens(int **grid, int row);
-int queen_safe(int row, int col);
-void create_coord(int row, int col);
-void free_array(int **grid);
-void print_coords(point *coords);
-void copy_coords(point *, point *);
-int permute_queens(int **grid, point *coords);
+#include "header.h"
 
 int ft_eight_queens_puzzle(void)
 {
